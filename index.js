4e12f49946e448fd6bf3971696e4e067d9d96968
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2022-01-16 22:25:39 -0300

    funcion crear modal

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const endpointPersonajes = "https://thronesapi.com/api/v2/Characters"
+const endpointPersonajes = "https://thronesapi.com/api/v2/Characters";
 const containerCards = document.querySelector(".cards");
 
 const pedirInfo = () => {
-    fetch(endpointPersonajes)
+  fetch(endpointPersonajes)
     .then((res) => res.json())
     .then((data) => {
-        crearTarjeta(data);
-    })
-}
+      crearTarjeta(data);
+    });
+};
 
 pedirInfo();
 
 const crearTarjeta = (data) => {
-
-    const mostrarEnHtml = data.reduce((acc, curr) =>{
-
-        return acc + 
-        `<div class="card" data-id="${curr.id}">
+  const mostrarEnHtml = data.reduce((acc, curr) => {
+    return (
+      acc +
+       `<div class="card" data-id="${curr.id}">
             <div class="container-img">
                 <img id="img-personaje" class="centered-img" src="${curr.imageUrl}" alt="">
             </div>
@@ -25,30 +24,73 @@ const crearTarjeta = (data) => {
             </div>
         </div>
         `
-    }, "");
+    );
+  }, "");
 
-    containerCards.innerHTML = mostrarEnHtml;
-    asignarClicksACards();
+  containerCards.innerHTML = mostrarEnHtml;
+  asignarClicksACards();
 };
 
 pedirInfo();
 
-const mostrarInfoPersonajes = (id) =>{
-    fetch(`https://thronesapi.com/api/v2/Characters/${id}`)
+const mostrarInfoPersonajes = (id) => {
+  fetch(`https://thronesapi.com/api/v2/Characters/${id}`)
     .then((res) => res.json())
-    .then((data) =>{
-        console.log(data)
-    })
-}
-    
-const asignarClicksACards = () =>{
-    const cards = document.querySelectorAll(".card");
-        
-    for (let i = 0; i< cards.length; i++) {
-        cards[i].onclick = () =>{
-            const idPersonajes = cards[i].dataset.id;
-            mostrarInfoPersonajes(idPersonajes);
-            console.log(idPersonajes)
-        }
-    }
-} 
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+const asignarClicksACards = () => {
+  const cards = document.querySelectorAll(".card");
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].onclick = () => {
+      const idPersonajes = cards[i].dataset.id;
+      mostrarInfoPersonajes(idPersonajes);
+      console.log(idPersonajes);
+    };
+  }
+};
+
+const arrayFamilias = [
+  {
+    name: "House Targaryen",
+    img: "targaryen_logo.png",
+  },
+  {
+    name: "House Stark",
+    img: "stark_logo.png",
+  },
+];
+
+const obtenerLogoFamilia = (familia) => {
+  const logo = arrayFamilias.find((elemento) => {
+    return elemento.name == familia;
+  });
+
+  return `./imgs/${logo.img}`;
+};
+
+const crearModal = (data) => {
+  return `<div class="modal" data-id="${data.id}">
+              <div class="container-boton-modal">
+                  <button class="abrir-cerrar-modal">Cerrar</button>
+              </div>
+              <div class="info-personaje">
+                  <div class="container-img-modal">
+                      <img class="img-modal" src="${data.imageUrl}" alt="">
+                  </div>
+                  <div class="info-principal">
+                      <img src="${obtenerLogoFamilia(data.family)}">
+                      <div class="datos">
+                          <p id="nombre-personaje">Fullname: ${
+                            data.firstName
+                          } ${data.lastName}</p>
+                          <p id="nombre-personaje">Title: ${data.title} </p>
+                          <p id="nombre-personaje">Family: ${data.family} </p>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
+};
