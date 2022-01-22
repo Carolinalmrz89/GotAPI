71000c9b921d32f8bf77fa91cd7ea3831701b889
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2022-01-21 21:28:16 -0300

    se crea array de objetos para logos de familias

--- houses.js
+++ houses.js
@@ -1,10 +1,135 @@
 const arrayFamilias = [
-    {
-      name: "House Targaryen",
-      img: "targaryen_logo.png",
-    },
-    {
-      name: "House Stark",
-      img: "stark_logo.png",
-    },
+  {
+    name: "House Targaryen",
+    img: "targaryen_logo.png",
+  },
+  {
+    name: "House Stark",
+    img: "stark_logo.png",
+  },
+  {
+    name: "Stark",
+    img: "stark_logo.png",
+  },
+  {
+    name: "House Lannister",
+    img: "lannister_logo.png",
+  },
+  {
+    name: "Lannister",
+    img: "lannister_logo.png",
+  },
+  {
+    name: "House Lanister",
+    img: "lannister_logo.png",
+  },
+
+  {
+    name: "House Baratheon",
+    img: "baratheon_logo.png",
+  },
+  {
+    name: "Baratheon",
+    img: "baratheon_logo.png",
+  },
+  {
+    name: "House Greyjoy",
+    img: "greyjoy_logo.png",
+  },
+  {
+    name: "Greyjoy",
+    img: "greyjoy_logo.png",
+  },
+  {
+    name: "House Tarly",
+    img: "tarly_logo.png",
+  },
+  {
+    name: "House Tyrell",
+    img: "tyrell_logo.png",
+  },
+  {
+    name: "Tyrell",
+    img: "tyrell_logo.png",
+  },
+  {
+    name: "House Clegane",
+    img: "clegane_logo.png",
+  },
+  {
+    name: "House Baelish",
+    img: "baelish_logo.png",
+  },
+  {
+    name: "House Seaworth",
+    img: "seaworth_logo.png",
+  },
+  {
+    name: "Free Folk",
+    img: "free_folk_logo.png",
+  },
+  {
+    name: "Tarth",
+    img: "tarth_logo.png",
+  },
+  {
+    name: "Naathi",
+    img: "noimg.png",
+  },
+  {
+    name: "Bolton",
+    img: "bolton_logo.png",
+  },
+  {
+    name: "Naharis",
+    img: "noimg.png",
+  },
+  {
+    name: "Lorath",
+    img: "noimg.png",
+  },
+  {
+    name: "Mormont",
+    img: "noimg.png",
+  },
+  {
+    name: "Sparrow",
+    img: "noimg.png",
+  },
+  {
+    name: "Viper",
+    img: "martell_logo.png",
+  },
+  {
+    name: "Sand",
+    img: "noimg.png",
+  },
+  {
+    name: "Worm",
+    img: "noimg.png",
+  },
+  {
+    name: "Qyburn",
+    img: "noimg.png",
+  },
+  {
+    name: "Bronn",
+    img: "bronn_logo.png",
+  },
+  {
+    name: "Unknown",
+    img: "noimg.png",
+  },
+  {
+    name: "None",
+    img: "noimg.png",
+  },
+  {
+    name: "Unkown",
+    img: "noimg.png",
+  },
+  {
+    name: "",
+    img: "noimg.png",
+  },
 ];
